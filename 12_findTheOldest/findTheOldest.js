3c17278aadddf2be5414dd61b3fea68bb6a8eb7c
lcastulo <lcaleandro@gmail>
2022-02-10 15:25:54 -0600

    Solved find the oldest with all test cases

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,18 @@
 const findTheOldest = function(people) {
-    const ordered = people.sort((a, b) => a.yearOfBirth > b.yearOfBirth ? 1: -1);
-    return ordered;
-};
+    const oldest = people.sort(function(a, b){
+      const getAge = function(birth, death) {
+        if (!death) {
+          death = new Date().getFullYear();
+        }
+        return death - birth;
+      };
+      
+      const lastPerson = getAge(a.yearOfBirth, a.yearOfDeath);
+      const nextPerson = getAge(b.yearOfBirth, b.yearOfDeath);
+      return lastPerson > nextPerson ? -1 : 1;
+    });
+    return oldest[0];
+  }
 
 // Do not edit below this line
 module.exports = findTheOldest;
