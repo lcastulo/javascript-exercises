2ffd6b44c63478aa2ea29d00c7cfe3cd99aee65b
lcastulo <lcaleandro@gmail>
2022-02-10 09:43:32 -0600

    Fibonacci solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  var a=1, b=0, temp;
 
+  while(num>=0){
+    temp = a;
+    a = a+b;
+    b = temp;
+    num--;
+  }
+  return b;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
