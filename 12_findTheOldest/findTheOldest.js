115396793cabecdbd1a48c1e07b0d0c98394ebdd
lcastulo <lcaleandro@gmail>
2022-02-10 14:33:16 -0600

    Solved findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const ordered = people.sort((a, b) => a.yearOfBirth > b.yearOfBirth ? 1: -1);
+    return ordered;
 };
 
 // Do not edit below this line
