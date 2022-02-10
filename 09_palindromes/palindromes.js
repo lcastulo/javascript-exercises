adbb53fe3c8320473df3344ee37a5f6ef1b8ce8d
lcastulo <lcaleandro@gmail.com>
2022-02-09 18:44:58 -0600

    Attempt 5  of palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,33 +1,28 @@
 const palindromes = function (str) {
-	var strarr = str.split("");
-	function onlyLetters(strarr){
-		let letterarr = []
+	function onlyLetters(str){
+		str.toLowerCase();
+		const strarr = str.split();
+		let letterarr = [];
 		for(i=0; i<strarr.length; i++){
 			if(strarr[i] === "a" || "b" || "c" || "d" || "e" || "f"
 				|| "g" || "h" || "i" || "j" || "k" || "l" || "m" || "n" 
 				|| "o" || "p" || "q" || "r" || "s" || "t" || "u" || "v"
 				|| "w" || "x" || "y" || "z"){
 					letterarr.push(strarr[i]);
-				}
+			}
 		}
 		return letterarr;
 	}
 	function isPalindrome(arr){
-		let firstElement = arr[0];
-		let lastElement = arr[arr.length-1];
-		if(firstElement != lastElement)
+		if(arr[0] != arr[arr.length-1])
 			return false;
-		if(arr.length === 0 || arr.length ===1)
+		if(arr.length <= 1)
 			return true;
-		if (firstElement === lastElement){
-			arr = arr.slice(1, arr.length-1);
-			return isPalindrome(arr);
-		}
-		
+		arr = arr.slice(1, arr.length-2);
+		return isPalindrome(arr);
 	}
 	
-	onlyLetters(str);
-	return isPalindrome(str);
+	return isPalindrome(onlyLetters(str));
 };
 
 // Do not edit below this line
